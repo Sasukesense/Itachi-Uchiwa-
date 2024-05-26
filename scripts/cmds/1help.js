e50e184ab06d1cb28da9e368aa776e39238b36e5
Sonic-Daniel <danielthiam54@gmail.com>
2024-05-26 23:18:23 +0100

    Update 1help.js

--- scripts/cmds/1help.js
+++ scripts/cmds/1help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🔱ʬɸʬ𝐀𝐐𝐖𝐀ʬɸʬ🔱";
+const doNotDelete = "❥❦𝑬𝑷𝑯𝑬𝑵𝑰𝑨๖ۣ•҉★";
 /** 
 * @author NTKhang
 * @author: do not delete it
